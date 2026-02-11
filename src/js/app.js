19cc605780663d6f011280c613fac095acabf280
Sebastian <zain-wave@outlook.com>
2026-02-11 14:44:02 -0500

    feat: basic modular project structure

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+import "../css/main.css";
+
+class App {
+  constructor() {
+    this.app = document.getElementById("app");
+    this.init();
+  }
+
+  init() {
+    this.app.innerHTML = "<h1>Start</h1><p>Melo</p>";
+  }
+}
+
+new App();
