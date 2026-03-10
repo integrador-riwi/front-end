593554132b34dd6415ade07abb461a0acf4e1d6a
Sebastian <zain-wave@outlook.com>
2026-03-09 19:25:01 -0500

    fix codereventselect

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import "../assets/styles/main.css";
-import "../assets/styles/coderEventSelect.css";
 import LoginView from "../views/LoginView.js";
 import DashboardView from "../views/DashboardView.js";
 import { isAuthenticated } from "../utils/auth.js";
@@ -61,13 +60,14 @@ class App {
         this.navigate("events");
         break;
       case "CODER":
-        console.log("hello");
         this.navigate("coderEventSelect");
         break;
       case "TL_DEVELOPMENT":
         this.navigate("coderEventSelect");
         break;
       case "TL_SOFT_SKILLS":
+        this.navigate("coderEventSelect");
+        break;
       case "TL_ENGLISH":
         this.navigate("coderEventSelect");
         break;
